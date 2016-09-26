7d4f68d118f58e7d88695bcd42d59312f16bdcc8
Yann Dubois <yann59.dubois@gmail.com>
2016-09-26 18:36:24 +0100

    simple nested call

--- TP1/display_stack.c
+++ TP1/display_stack.c
@@ -1,3 +1,20 @@
+#include <stdio.h>
+
+void display_stack(long int esp, long int ebp) {
+  printf("esp : %#010x\t ebp : %#010x\n", esp, ebp);
+}
+
+void nothing_to_do() {
+  long int esp, ebp;
+
+  asm("movl %%esp, %0"
+        : "=r" (esp));
+  asm("movl %%ebp, %0"
+        : "=r" (ebp));
+
+  display_stack(esp, ebp);
+}
+
 int main () {
   long int esp, ebp;
 
@@ -6,5 +23,12 @@ int main () {
   asm("movl %%ebp, %0"
         : "=r" (ebp));
 
-  printf("esp : %#010x\t ebp : %#010x\n", esp, ebp);
+  display_stack(esp, ebp);
+
+  asm("movl %%esp, %0"
+        : "=r" (esp));
+  asm("movl %%ebp, %0"
+        : "=r" (ebp));
+
+  nothing_to_do();
 }
