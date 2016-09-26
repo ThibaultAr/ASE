1a914683139de445bddc75c86f56f31d964a008e
Yann Dubois <yann59.dubois@gmail.com>
2016-09-26 19:09:18 +0100

    Add rec function and print its param

--- TP1/display_stack.c
+++ TP1/display_stack.c
@@ -11,8 +11,25 @@ void nothing_to_do() {
         : "=r" (esp));
   asm("movl %%ebp, %0"
         : "=r" (ebp));
-
+  printf("func: nothing_to_do ");
   display_stack(esp, ebp);
+}
+
+int useless_cpt(int n) {
+  long int esp, ebp;
+
+  asm("movl %%esp, %0"
+        : "=r" (esp));
+  asm("movl %%ebp, %0"
+        : "=r" (ebp));
+  printf("func: useless_cpt ");
+  display_stack(esp, ebp);
+  printf("param : %#010x\n", &n);
+
+  if(n == 0)
+    return n;
+  else
+    1 + useless_cpt(n - 1);
 }
 
 int main () {
@@ -24,11 +41,7 @@ int main () {
         : "=r" (ebp));
 
   display_stack(esp, ebp);
-
-  asm("movl %%esp, %0"
-        : "=r" (esp));
-  asm("movl %%ebp, %0"
-        : "=r" (ebp));
-
   nothing_to_do();
+  useless_cpt(3);
+
 }
