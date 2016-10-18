bcec638a2e65631f27b9a2920a43f9153e1c05ca
Thibault Arloing <thibault.arloing@hotmail.fr>
2016-10-18 18:15:43 +0200

    initial commit for TP4

---
+++ TP4/drive.c
@@ -0,0 +1,60 @@
+#include "drive.h"
+
+void goto_sector(unsigned int cylinder, unsigned int sector) {
+  _out(HDA_DATAREGS, (cylinder >> 8) & 0xFF);
+  _out(HDA_DATAREGS + 1, cylinder & 0xFF);
+  _out(HDA_DATAREGS + 2, (sector >> 8) & 0xFF);
+  _out(HDA_DATAREGS + 3, sector & 0xFF);
+  _out(HDA_CMDREG, CMD_SEEK);
+  _sleep(HDA_IRQ);
+}
+
+void read_sector(unsigned int cylinder, unsigned int sector, unsigned char *buffer) {
+  goto_sector(cylinder, sector);
+  _out(HDA_DATAREGS, 0);
+  _out(HDA_DATAREGS + 1, 1);
+  _out(HDA_CMDREG, CMD_READ);
+  _sleep(HDA_IRQ);
+  memcpy(buffer, MASTERBUFFER, SECTORSIZE);
+}
+
+void write_sector(unsigned int cylinder, unsigned int sector, const unsigned char *buffer) {
+  memcpy(MASTERBUFFER, buffer, SECTORSIZE);
+  goto_sector(cylinder, sector);
+  _out(HDA_DATAREGS, 0);
+  _out(HDA_DATAREGS, 1);
+  _out(HDA_CMDREG, CMD_WRITE);
+  _sleep(HDA_IRQ);
+}
+
+void format_sector(unsigned int cylinder, unsigned int sector, unsigned int nsector, unsigned int value) {
+  int i;
+  goto_sector(cylinder, sector);
+  for(i = 0; i < nsector; i++) {
+    _out(HDA_DATAREGS, 0);
+    _out(HDA_DATAREGS + 1, 1);
+    _out(HDA_DATAREGS + 2, nsector);
+    _out(HDA_CMDREG, CMD_FORMAT);
+    _sleep(HDA_IRQ);
+  }
+}
+
+void check_dskinfo(unsigned int cylinder, unsigned int sector) {
+  int nbCyl, nbSec, tailleSec;
+  _out(HDA_CMDREG, CMD_DSKINFO);
+  nbCyl = _in(HDA_DATAREGS);
+  nbCyl = (nbCyl << 8) + _in(HDA_DATAREGS + 1);
+  nbSec = _in(HDA_DATAREGS + 2);
+  nbSec = (nbSec << 8) + _in(HDA_DATAREGS + 3);
+  tailleSec = _in(HDA_DATAREGS + 4);
+  tailleSec = (tailleSec << 8) + _in(HDA_DATAREGS + 5);
+
+  if(tailleSec != SECTORSIZE)
+    printf("Recompilez le noyau avec une taille de secteur de : %d\n", tailleSec);
+
+  if(cylinder > nbCyl || cylinder > 0xFF)
+    printf("L'indice du cylindre est trop grand ou n'est pas sur 16 bits\n");
+
+  if(sector > nbSec || sector > 0xFF)
+    printf("L'indice du secteur est trop grand ou n'est pas sur 16 bits\n");
+}
---
+++ TP4/drive.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "hardware.h"
+
+#define SECTORSIZE 256
+#define HDA_CMDREG 0x3F6
+#define HDA_DATAREGS 0x110
+#define HDA_IRQ 14
+
+void read_sector(unsigned int cylinder, unsigned int sector, unsigned char *buffer);
+
+void write_sector(unsigned int cylinder, unsigned int sector, const unsigned char *buffer);
+
+void format_sector(unsigned int cylinder, unsigned int sector, unsigned int nsector, unsigned int value);
+
+void check_dskinfo(unsigned int cylinder, unsigned int sector);
