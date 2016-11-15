86c317f7dbbdc2d1c36cf80f021f67b7c657dc75
Thibault Arloing <thibault.arloing@hotmail.fr>
2016-11-15 17:56:16 +0100

    inode

--- TP4/bloc.c
+++ TP4/bloc.c
@@ -61,3 +61,75 @@ void free_bloc (unsigned int bloc) {
 
   super.super_firstFree = bloc;
 }
+
+void read_bloc_n(unsigned int nBloc, unsigned char * buffer, unsigned int size){
+  read_sector_n(cylinder_of_bloc(current_vol, nBloc), sector_of_bloc(current_vol, nBloc), buffer, size);
+}
+
+void write_bloc_n(unsigned int nBloc, unsigned char * buffer, unsigned int size){
+  write_sector_n(cylinder_of_bloc(current_vol, nBloc), sector_of_bloc(current_vol, nBloc), buffer, size);
+}
+
+void read_inode(unsigned int inumber, struct inode_s inode){
+  read_bloc_n(inumber, &inode, sizeof(struct inode_s));
+}
+
+void write_inode(unsigned int inumber, struct inode_s inode){
+  write_bloc_n(inumber, &inode, sizeof(struct inode_s));
+}
+
+unsigned int create_inode(enum file_type_e type){
+  struct inode_s inode;
+  unsigned int bloc;
+  inode.i_type = type;
+  inode.i_size = 0;
+  for(int i = 0; i < NDIRECT; i++)
+    inode.i_direct[i] = 0;
+  inode.i_indirect = 0;
+  inode.i_2indirect = 0;
+  bloc = new_bloc();
+  write_inode(bloc, &inode);
+
+  return bloc;
+}
+
+void free_blocs(unsigned int blocs[], unsigned int size){
+    for(int i = 0; i < size; i++)
+      if(blocs[i])
+        free_bloc(blocs[i]);
+}
+
+void delete_inode(unsigned int inumber){
+    struct inode_s inode;
+    read_inode(inumber, &inode);
+
+    free_blocs(inode.i_direct, NDIRECT);
+    if(inode.i_indirect){
+      unsigned int bloc[NNBPB];
+      read_bloc_n(inode.i_indirect, bloc, sizeof(unsigned int));
+      free_blocs(bloc, NNBPB);
+      free_bloc(inode.inode_indirect);
+    }
+
+    if(inode.i_2indirect){
+      unsigned int bloc2[NNBPB];
+      read_bloc_n(inode.i_2indirect, bloc2, sizeof(unsigned int));
+      for(int b = 0; b < NNBPB; b++){
+        if(bloc2[b]){
+          unsigned int bloc[NNBPB];
+          read_bloc_n(bloc2[b], bloc, sizeof(unsigned int));
+          free_blocs(bloc, NNBPB);
+          free_bloc(bloc2[b]);
+        }
+      }
+      free_bloc(inode.i_2indirect);
+    }
+    free_bloc(inumber);
+}
+
+unsigned int vbloc_of_fbloc(unsigned int inumber, unsigned int fbloc) {
+  struct inode_s inode;
+  read_inode(inumber, &inode);
+  if(fbloc < NDIRECT)
+    return inode.i_direct[fbloc];
+}
